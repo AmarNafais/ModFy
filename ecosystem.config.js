1a11e03eca7328cfb554c5b2ae080dbd3d97c3b2
AmarNafais <amarnafais@gmail.com>
2025-11-09 18:01:14 +0530

    test2

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    name: "modfy-server",
+    script: "./server/index.ts",
+    interpreter: "node",
+    interpreter_args: "-r ts-node/register",
+    env: {
+      NODE_ENV: "production",
+      PORT: 3000,
+      DB_HOST: "127.0.0.1",
+      DB_USER: "root",
+      DB_PASSWORD: "Complex123", // Replace with your actual MySQL password
+      DB_NAME: "modfy"
+    },
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G"
+  }]
+};
