a13b3fbb834d66d728753921aa4a1cab008c6534
NabeelMohideen <nabeelnabeela8@gmail.com>
2025-12-18 22:19:34 +0530

    Logo size change

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -126,7 +126,7 @@ export default function Header({ onCartOpen }: HeaderProps) {
           {/* Brand Logo */}
           <div className="flex-shrink-0">
             <Link href="/" data-testid="link-home" className="flex items-center">
-              <img src="/storage/logo/logo.svg" alt="ModFy" className="h-8" />
+              <img src="/storage/logo/logo.svg" alt="ModFy" className="h-12" />
             </Link>
           </div>
 
